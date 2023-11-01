ac52ff97759e5e09d81427338760343bd75cc779
colinpaint <88828347+colinpaint@users.noreply.github.com>
2023-11-01 18:46:54 +0000

    fold

--- src/xop/RtspServer.cpp
+++ src/xop/RtspServer.cpp
@@ -9,7 +9,7 @@ using namespace std;
 
 //{{{
 RtspServer::RtspServer (EventLoop* loop)
-	: TcpServer(loop)
+  : TcpServer(loop)
 {
 
 }
@@ -24,96 +24,96 @@ RtspServer::~RtspServer()
 //{{{
 std::shared_ptr<RtspServer> RtspServer::Create (xop::EventLoop* loop)
 {
-	std::shared_ptr<RtspServer> server (new RtspServer(loop));
-	return server;
+  std::shared_ptr<RtspServer> server (new RtspServer(loop));
+  return server;
 }
 //}}}
 
 //{{{
 MediaSessionId RtspServer::AddSession (MediaSession* session)
 {
-		std::lock_guard<std::mutex> locker(mutex_);
+    std::lock_guard<std::mutex> locker(mutex_);
 
-		if (rtsp_suffix_map_.find(session->GetRtspUrlSuffix()) != rtsp_suffix_map_.end()) {
-				return 0;
-		}
+    if (rtsp_suffix_map_.find(session->GetRtspUrlSuffix()) != rtsp_suffix_map_.end()) {
+        return 0;
+    }
 
-		std::shared_ptr<MediaSession> media_session(session);
-		MediaSessionId sessionId = media_session->GetMediaSessionId();
-	rtsp_suffix_map_.emplace(std::move(media_session->GetRtspUrlSuffix()), sessionId);
-	media_sessions_.emplace(sessionId, std::move(media_session));
+    std::shared_ptr<MediaSession> media_session(session);
+    MediaSessionId sessionId = media_session->GetMediaSessionId();
+  rtsp_suffix_map_.emplace(std::move(media_session->GetRtspUrlSuffix()), sessionId);
+  media_sessions_.emplace(sessionId, std::move(media_session));
 
-		return sessionId;
+    return sessionId;
 }
 //}}}
 //{{{
 void RtspServer::RemoveSession (MediaSessionId sessionId)
 {
-		std::lock_guard<std::mutex> locker(mutex_);
+    std::lock_guard<std::mutex> locker(mutex_);
 
-		auto iter = media_sessions_.find(sessionId);
-		if(iter != media_sessions_.end()) {
-				rtsp_suffix_map_.erase(iter->second->GetRtspUrlSuffix());
-				media_sessions_.erase(sessionId);
-		}
+    auto iter = media_sessions_.find(sessionId);
+    if(iter != media_sessions_.end()) {
+        rtsp_suffix_map_.erase(iter->second->GetRtspUrlSuffix());
+        media_sessions_.erase(sessionId);
+    }
 }
 //}}}
 
 //{{{
 MediaSession::Ptr RtspServer::LookMediaSession (const std::string& suffix)
 {
-		std::lock_guard<std::mutex> locker(mutex_);
+    std::lock_guard<std::mutex> locker(mutex_);
 
-		auto iter = rtsp_suffix_map_.find(suffix);
-		if(iter != rtsp_suffix_map_.end()) {
-				MediaSessionId id = iter->second;
-				return media_sessions_[id];
-		}
+    auto iter = rtsp_suffix_map_.find(suffix);
+    if(iter != rtsp_suffix_map_.end()) {
+        MediaSessionId id = iter->second;
+        return media_sessions_[id];
+    }
 
-		return nullptr;
+    return nullptr;
 }
 //}}}
 //{{{
 MediaSession::Ptr RtspServer::LookMediaSession (MediaSessionId session_Id)
 {
-		std::lock_guard<std::mutex> locker(mutex_);
+    std::lock_guard<std::mutex> locker(mutex_);
 
-		auto iter = media_sessions_.find(session_Id);
-		if(iter != media_sessions_.end()) {
-				return iter->second;
-		}
+    auto iter = media_sessions_.find(session_Id);
+    if(iter != media_sessions_.end()) {
+        return iter->second;
+    }
 
-		return nullptr;
+    return nullptr;
 }
 //}}}
 
 //{{{
 bool RtspServer::PushFrame (MediaSessionId session_id, MediaChannelId channel_id, AVFrame frame)
 {
-		std::shared_ptr<MediaSession> sessionPtr = nullptr;
+    std::shared_ptr<MediaSession> sessionPtr = nullptr;
 
-		{
-				std::lock_guard<std::mutex> locker(mutex_);
-				auto iter = media_sessions_.find(session_id);
-				if (iter != media_sessions_.end()) {
-						sessionPtr = iter->second;
-				}
-				else {
-						return false;
-				}
-		}
+    {
+        std::lock_guard<std::mutex> locker(mutex_);
+        auto iter = media_sessions_.find(session_id);
+        if (iter != media_sessions_.end()) {
+            sessionPtr = iter->second;
+        }
+        else {
+            return false;
+        }
+    }
 
-		if (sessionPtr!=nullptr && sessionPtr->GetNumClient()!=0) {
-				return sessionPtr->HandleFrame(channel_id, frame);
-		}
+    if (sessionPtr!=nullptr && sessionPtr->GetNumClient()!=0) {
+        return sessionPtr->HandleFrame(channel_id, frame);
+    }
 
-		return false;
+    return false;
 }
 //}}}
 
 //{{{
 TcpConnection::Ptr RtspServer::OnConnect (SOCKET sockfd)
 {
-	return std::make_shared<RtspConnection>(shared_from_this(), event_loop_->GetTaskScheduler().get(), sockfd);
+  return std::make_shared<RtspConnection>(shared_from_this(), event_loop_->GetTaskScheduler().get(), sockfd);
 }
 //}}}
--- src/xop/VP8Source.cpp
+++ src/xop/VP8Source.cpp
@@ -1,49 +1,57 @@
-
+//{{{  includes
 #if defined(WIN32) || defined(_WIN32)
-#ifndef _CRT_SECURE_NO_WARNINGS
-#define _CRT_SECURE_NO_WARNINGS
-#endif
+  #ifndef _CRT_SECURE_NO_WARNINGS
+    #define _CRT_SECURE_NO_WARNINGS
+  #endif
 #endif
 
 #include "VP8Source.h"
 #include <cstdio>
 #include <chrono>
-#if defined(__linux) || defined(__linux__)
-#include <sys/time.h>
-#endif
 
+#if defined(__linux) || defined(__linux__)
+  #include <sys/time.h>
+#endif
+//}}}
 using namespace xop;
 using namespace std;
 
+//{{{
 VP8Source::VP8Source(uint32_t framerate)
     : framerate_(framerate)
 {
     payload_ = 96;
     clock_rate_ = 90000;
 }
-
+//}}}
+//{{{
 VP8Source* VP8Source::CreateNew(uint32_t framerate)
 {
     return new VP8Source(framerate);
 }
-
+//}}}
+//{{{
 VP8Source::~VP8Source()
 {
 
 }
+//}}}
 
+//{{{
 string VP8Source::GetMediaDescription(uint16_t port)
 {
     char buf[100] = { 0 };
     sprintf(buf, "m=video %hu RTP/AVP 96", port);
     return string(buf);
 }
-
+//}}}
+//{{{
 string VP8Source::GetAttribute()
 {
     return string("a=rtpmap:96 VP8/90000");
 }
-
+//}}}
+//{{{
 bool VP8Source::HandleFrame(MediaChannelId channel_id, AVFrame frame)
 {
     uint8_t* frame_buf = frame.buffer.get();
@@ -53,7 +61,7 @@ bool VP8Source::HandleFrame(MediaChannelId channel_id, AVFrame frame)
         frame.timestamp = GetTimestamp();
     }
 
-    // X = R = N = 0; PartID = 0; 
+    // X = R = N = 0; PartID = 0;
     // S = 1 if this is the first (or only) fragment of the frame
     uint8_t vp8_payload_descriptor = 0x10;
 
@@ -75,7 +83,7 @@ bool VP8Source::HandleFrame(MediaChannelId channel_id, AVFrame frame)
         rtp_pkt.data.get()[RTP_TCP_HEAD_SIZE + RTP_HEADER_SIZE + 0] = vp8_payload_descriptor;
         memcpy(rtp_pkt.data.get() + RTP_TCP_HEAD_SIZE + RTP_HEADER_SIZE + RTP_VPX_HEAD_SIZE, frame_buf, payload_size);
 
-        if (send_frame_callback_) {           
+        if (send_frame_callback_) {
             if (!send_frame_callback_(channel_id, rtp_pkt))
                 return false;
         }
@@ -87,9 +95,12 @@ bool VP8Source::HandleFrame(MediaChannelId channel_id, AVFrame frame)
 
     return true;
 }
+//}}}
 
+//{{{
 uint32_t VP8Source::GetTimestamp()
 {
     auto time_point = chrono::time_point_cast<chrono::microseconds>(chrono::steady_clock::now());
     return (uint32_t)((time_point.time_since_epoch().count() + 500) / 1000 * 90);
 }
+//}}}
